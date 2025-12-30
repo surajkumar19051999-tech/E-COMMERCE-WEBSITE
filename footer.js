db45dcc0da1e038311392a7635a391627cd01ecf
Suraj kumar <surajkumar19051999@gmail.com>
2025-12-31 00:26:35 +0530

    adde dsa

--- footer.js
+++ footer.js
@@ -1,1 +1,15 @@
-footer
+function largest(arr) {
+    let max = arr[0];
+
+    // Traverse from second and compare
+    // every element with current max
+    for (let i = 1; i < arr.length; i++)
+        if (arr[i] > max)
+            max = arr[i];
+
+    return max;
+}
+
+// Driver Code
+const arr = [10, 324, 45, 90, 9808];
+console.log(largest(arr));
