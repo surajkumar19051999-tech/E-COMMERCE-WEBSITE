b947a9644b2402250e35195edf49c22140f1071b
Suraj kumar <surajkumar19051999@gmail.com>
2025-12-31 00:30:33 +0530

    commneted code removed

--- footer.js
+++ footer.js
@@ -1,8 +1,7 @@
 function largest(arr) {
     let max = arr[0];
 
-    // Traverse from second and compare
-    // every element with current max
+    
     for (let i = 1; i < arr.length; i++)
         if (arr[i] > max)
             max = arr[i];
@@ -10,6 +9,6 @@ function largest(arr) {
     return max;
 }
 
-// Driver Code
+
 const arr = [10, 324, 45, 90, 9808];
 console.log(largest(arr));
